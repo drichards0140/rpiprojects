29610bccce68ce84cf1762875c5f32922960d140
D Richards <drichards0140@gmail.com>
2017-09-10 08:08:33 -0400

    updating organization of files
    updating organization of files

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+import time
+import signal # ?
+import scrollphathd
+from scrollphathd.fonts import font3x5
+
+scrollphathd.rotate(180) #rotates display for upside down orientation of phathd
+scrollphathd.write_string('hello world !!! ', x=0, y=1, font=font3x5, brightness=0.2) 3
+while True:
+    scrollphathd.show()
+    scrollphathd.scroll()
+    time.sleep(0.01) #
